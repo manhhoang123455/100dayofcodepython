ab9bb8cd98e330d8111186ac6f1a5f09dcec603a
hoang_phan <hoang.phan@hitachivantara.com>
2022-01-10 00:02:25 +0700

    Day 1

---
+++ day 1... 09012022/the_name_of_band.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator')
+city = input('what\'s of the city you grew up in?\n')
+name_of_the_pet = input("What's your pet's name?\n")
+print(f"your band name cloud be {city} {name_of_the_pet}")
