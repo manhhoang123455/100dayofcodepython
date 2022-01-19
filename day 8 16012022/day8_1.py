da9323be94553399a18d554f661e3441e656e86c
hoang_phan <hoang.phan@hitachivantara.com>
2022-01-20 04:04:44 +0700

    day 8

---
+++ day 8 16012022/day8_1.py
@@ -0,0 +1,13 @@
+# def greet():
+#     print("hello")
+#     print("how are you?")
+
+
+# function that allows for input
+
+def greet_with_name(name):
+    print(f"hello {name}")
+    print(f"How are you, {name}??")
+
+
+greet_with_name("Hoang")
---
+++ day 8 16012022/test.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(start_text,shift,dict):
+    end_text = ''
+    if dict == 'decode':
+        shift = shift * -1
+    for letter in start_text:
+        possition = alphabet.index(letter)
+       
+        end_text += alphabet[possition + shift]
+    print(end_text)
+
+
+caesar(text,shift,direction)
