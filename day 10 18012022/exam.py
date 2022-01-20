3342d46bb1d3f4175f2283a2d40a7a6230a1a872
hoang_phan <hoang.phan@hitachivantara.com>
2022-01-21 04:14:39 +0700

    day 10

---
+++ day 10 18012022/exam.py
@@ -0,0 +1,12 @@
+## Function with Output
+
+
+def format_name(f_name,l_name):
+    f_name = f_name.title()
+    l_name = l_name.title()
+    return f"{f_name} {l_name}"
+
+
+
+name = format_name('hoang','phan')
+print(name)
